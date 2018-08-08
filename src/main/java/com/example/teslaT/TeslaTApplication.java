3f56a9299c056d9ff9b0c1b17a606c3212da4c8e
inusasunny <inusasunny@gmail.com>
2018-08-08 09:43:36 -0500

    initial commit

---
+++ src/main/java/com/example/teslaT/TeslaTApplication.java
@@ -0,0 +1,14 @@
+package com.example.teslaT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeslaTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeslaTApplication.class, args);
+	}
+}
+
+
